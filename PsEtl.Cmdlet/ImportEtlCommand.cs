f290c67f9052395108763bbe2314c65645b8cd7a
jehurren@microsoft.com <jehurren@microsoft.com>
2022-09-08 09:48:54 -0600

    Add powershell cmdlet

---
+++ PsEtl.Cmdlet/ImportEtlCommand.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading;
+
+namespace PsEtl.Cmdlet
+{
+    [Cmdlet(VerbsData.Import, "Etl")]
+    [OutputType(typeof(TraceData))]
+    public class ImportEtlCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = false)]
+        public string Path { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var source = new ETWTraceEventSource(Path);
+            int eventId = 0;
+
+            source.Dynamic.All += delegate (TraceEvent data)
+            {
+                var traceData = new TraceData
+                {
+                    Id = Interlocked.Increment(ref eventId),
+                    ActivityId = data.ActivityID,
+                    RelatedActivityId = data.RelatedActivityID,
+                    EventName = data.EventName,
+                    Level = data.Level,
+                    ProviderName = data.ProviderName,
+                    ProcessId = data.ProcessID,
+                    ThreadId = data.ThreadID,
+                    TimeStampRelativeMSec = data.TimeStampRelativeMSec
+                };
+
+                for (int i = 0; i < data.PayloadNames.Length; i++)
+                {
+                    traceData.Payload.Add(data.PayloadNames[i], data.PayloadString(i));
+                }
+
+                WriteObject(traceData);
+            };
+            source.Process();
+
+        }
+    }
+}
---
+++ PsEtl.Cmdlet/TraceData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+
+namespace PsEtl.Cmdlet
+{
+    public class TraceData
+    {
+        public int Id { get; internal set; }
+        public double TimeStampRelativeMSec { get; internal set; }
+        public Guid ActivityId { get; internal set; }
+        public Guid RelatedActivityId { get; internal set; }
+        public string ProviderName { get; internal set; }
+        public int ProcessId { get; internal set; }
+        public int ThreadId { get; internal set; }
+        public string EventName { get; internal set; }
+        public TraceEventLevel Level { get; internal set; }
+        public Dictionary<string, string> Payload { get; internal set; } = new Dictionary<string, string>();
+    }
+}
